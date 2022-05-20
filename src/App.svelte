7ece90aa062cc954d44da15b9a19c11b2740a865
Paul Adams <ghusiyy@gmail.com>
2022-05-20 02:45:49 -0700

    rough sketch of game board

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,24 @@
 <main>
   <article>
     <section>
-      <div class="game-board">game board</div>
+      <div class="game-board">
+        <div class="mancala_b" />
+        <div class="pit-wrapper">
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+          <div class="pit" />
+        </div>
+        <div class="mancala_a" />
+      </div>
     </section>
   </article>
 </main>
@@ -25,12 +42,32 @@
     margin: 0 auto;
   }
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
+  .game-board {
+    display: flex;
+    justify-content: space-between;
+    height: 500px;
+    width: 700px;
+    border: 1px solid black;
+  }
 
-    p {
+  .mancala_a,
+  .mancala_b {
+    border: 1px solid blue;
+    width: 25%;
+  }
+
+  .pit-wrapper {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
+  .pit {
+    border: 1px solid red;
+  }
+
+  @media (min-width: 480px) {
+    h3 {
       max-width: none;
     }
   }

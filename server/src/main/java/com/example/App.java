e345faefa3368592a6c26d798259c5ca82fcda77
Paul Adams <ghusiyy@gmail.com>
2022-05-20 02:14:50 -0700

    add server module

---
+++ server/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7001);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}

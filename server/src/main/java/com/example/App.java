e48be43ffbd00c29a37eca0cf3719b7da885fb8b
Paul Adams <ghusiyy@gmail.com>
2022-05-31 02:10:36 -0700

    remove Example object

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -9,9 +9,8 @@ public class App {
         app.ws("/game", ws -> {
             ws.onConnect(ctx -> ctx.send("connected to server"));
             ws.onMessage(ctx -> {
-                // GameConfig config = new GameConfig("fred", "guest");
-                Example ex = ctx.messageAsClass(Example.class);
-                System.out.println(ex.p1Name);
+                GameConfig ex = ctx.messageAsClass(GameConfig.class);
+                System.out.println(ex.getP1Name());
             });
         });
     }
--- server/src/main/java/com/example/Example.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Example {
-    public String p1Name;
-    public String p2Name;
-
-    public Example() {
-    }
-}
